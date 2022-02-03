fbd7fed2bdc74ef2e41a2b035a8021c68697e17b
Lukasz <lukasz.gawel@gmail.com>
2022-02-03 23:34:37 +0100

    Added database converters for Currency

---
+++ accounts-service/src/main/java/com/lukgaw/bank/accounts/boundary/out/r2dbc/converters/CurrencyWritingConverter.java
@@ -0,0 +1,17 @@
+package com.lukgaw.bank.accounts.boundary.out.r2dbc.converters;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.WritingConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Currency;
+
+@WritingConverter
+@Component
+public class CurrencyWritingConverter implements Converter<Currency, String> {
+
+    @Override
+    public String convert(Currency source) {
+        return source.getCurrencyCode();
+    }
+}

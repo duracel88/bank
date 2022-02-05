50ecae6671cbf423f0f735ec832cc5ff404e5b59
Lukasz <lukasz.gawel@gmail.com>
2022-02-05 15:42:49 +0100

    Removed issuer-uri to make stack work across all domain \ Added jwks url to app able to validat token

--- accounts-service/src/main/java/com/lukgaw/bank/accounts/configuration/JWTSecurityConfiguration.java
+++ accounts-service/src/main/java/com/lukgaw/bank/accounts/configuration/JWTSecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -21,11 +22,14 @@ public class JWTSecurityConfiguration {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http,
-                                                         ReactiveJwtAuthenticationConverterAdapter authenticationConverter) {
+                                                         ReactiveJwtAuthenticationConverterAdapter authenticationConverter,
+                                                         ReactiveJwtDecoder reactiveJwtDecoder) {
         return http
                 .authorizeExchange(ex -> ex.anyExchange().authenticated())
                 .oauth2ResourceServer(oAuth -> oAuth
-                        .jwt(jwt -> jwt.jwtAuthenticationConverter(authenticationConverter)))
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(authenticationConverter)
+                                .jwtDecoder(reactiveJwtDecoder)))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .build();
     }

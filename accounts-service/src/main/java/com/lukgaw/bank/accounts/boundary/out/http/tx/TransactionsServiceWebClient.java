b55845480511a9ecfb6d0fd76c10fc65a989b21d
Lukasz <lukasz.gawel@gmail.com>
2022-02-06 11:49:33 +0100

    Backoff for tx-service

--- accounts-service/src/main/java/com/lukgaw/bank/accounts/boundary/out/http/tx/TransactionsServiceWebClient.java
+++ accounts-service/src/main/java/com/lukgaw/bank/accounts/boundary/out/http/tx/TransactionsServiceWebClient.java
@@ -1,27 +1,39 @@
 package com.lukgaw.bank.accounts.boundary.out.http.tx;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
 import java.util.Map;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 public class TransactionsServiceWebClient implements TransactionsService {
     private final WebClient webClient;
+    private final Settings settings;
+
+    @AllArgsConstructor(staticName = "of")
+    public static final class Settings {
+        private int maxAttempts = 3;
+        private int backoffSeconds = 1;
+    }
 
     @Override
     public Mono<AccountTransactionsDTO> getAccountTransactions(UUID accountId, Pageable pageRequest) {
         return webClient.get().uri("/transactions/{accountId}", Map.of("accountId", accountId))
-                .exchangeToMono(res -> res.bodyToMono(AccountTransactionsDTO.class));
+                .exchangeToMono(res -> res.bodyToMono(AccountTransactionsDTO.class))
+                .retryWhen(Retry.backoff(settings.maxAttempts, Duration.ofSeconds(settings.backoffSeconds)));
     }
 
     @Override
     public Mono<DepositResponseDTO> makeDeposit(DepositRequestDTO request) {
         return webClient.post().uri("/deposit")
                 .bodyValue(request)
-                .exchangeToMono(res -> res.bodyToMono(DepositResponseDTO.class));
+                .exchangeToMono(res -> res.bodyToMono(DepositResponseDTO.class))
+                .retryWhen(Retry.backoff(settings.maxAttempts, Duration.ofSeconds(settings.backoffSeconds)));
     }
 }
--- accounts-service/src/main/java/com/lukgaw/bank/accounts/configuration/RestServicesConfiguration.java
+++ accounts-service/src/main/java/com/lukgaw/bank/accounts/configuration/RestServicesConfiguration.java
@@ -1,5 +1,6 @@
 package com.lukgaw.bank.accounts.configuration;
 
+import com.lukgaw.bank.accounts.boundary.out.http.LoggingWebClient;
 import com.lukgaw.bank.accounts.boundary.out.http.tx.TransactionsService;
 import com.lukgaw.bank.accounts.boundary.out.http.tx.TransactionsServiceWebClient;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -14,11 +15,13 @@ public class RestServicesConfiguration {
     @Bean
     @Qualifier("Tx-service")
     public WebClient transactionsWebClient(@Value("${services.tx-service.host}") String baseUrl) {
-        return WebClient.builder().baseUrl(baseUrl).build();
+        return new LoggingWebClient(baseUrl);
     }
 
     @Bean
-    public TransactionsService transactionsService(@Qualifier("Tx-service") WebClient webClient) {
-        return new TransactionsServiceWebClient(webClient);
+    public TransactionsService transactionsService(@Qualifier("Tx-service") WebClient webClient,
+                                                   @Value("${services.tx-service.retry.backoff.maxAttempts}") int maxAttempts,
+                                                   @Value("${services.tx-service.retry.backoff.backoffSeconds}") int backoffSeconds) {
+        return new TransactionsServiceWebClient(webClient, TransactionsServiceWebClient.Settings.of(maxAttempts, backoffSeconds));
     }
 }
